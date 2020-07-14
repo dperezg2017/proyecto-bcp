5413bd61e625aadefddfbf93543ba3881344cbc1
dperezg2017 <dperez@tci.net.pe>
2020-07-13 19:33:38 -0500

    inicio del proy

---
+++ cambiarMoneda/src/main/java/com/example/bcp/CambiarMonedaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambiarMonedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambiarMonedaApplication.class, args);
+	}
+
+}
