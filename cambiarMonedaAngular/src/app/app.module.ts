daf4f7f39ab1148f06b2c67e2a8f248df014a5a8
dperezg2017 <dperez@tci.net.pe>
2020-07-16 00:51:10 -0500

    finish

--- cambiarMonedaAngular/src/app/app.module.ts
+++ cambiarMonedaAngular/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipocambioComponent } from './tipocambio/tipocambio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TipocambioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
